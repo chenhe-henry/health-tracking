e92095a97db5e8cfe5771ef3fc50af67955d46b0
chenhe-henry <chenhe.elec@gmail.com>
2020-07-13 16:02:30 +1000

    fetch api for general status for covid-19

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,34 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>COVID-19 Tracker</h1>
+    <h1>Last updated @ {{generalCase.last_update}}</h1>
+    <h1>Total cases: {{generalCase.total_cases}}</h1>
+    <h1>Currently infected: {{generalCase.currently_infected}}</h1>
+    <h1>Total recovered: {{generalCase.recovery_cases}}</h1>
+    <h1>Total death: {{generalCase.death_cases}}</h1>
   </div>
 </template>
 
 <script>
+import CaseService from "@/services/CaseService.js";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      generalCase: {}
+    };
+  },
+  created() {
+    CaseService.getGeneralInfo()
+      .then(res => {
+        this.generalCase = res.data.data;
+        console.log(res.data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/services/CaseService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "https://corona-virus-stats.herokuapp.com/api/v1/cases/",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  getGeneralInfo() {
+    return apiClient.get("general-stats");
+  },
+};
